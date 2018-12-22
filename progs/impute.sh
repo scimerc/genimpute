acba299aab1f9d6ca4122678b585dbef723a3420
Florian Krull <flo.krull@gmail.com>
2018-12-22 22:59:11 +0100

    optimize sbatch settings for phasing and impute - phasing: 3h, batch3 - imputation: 24h, batch3 - generate script for initial conversion (to run on cluster) - split merge script generation from imputation loop - monitor CPU and mem usage with /usr/bin/time

--- progs/impute.sh
+++ progs/impute.sh
@@ -7,51 +7,59 @@ declare -r BASEDIR="$( cd "$( dirname $0 )" && cd .. && pwd )"
 export BASEDIR
 
 execmode="slurm"
-# this time is for imputation - expected time for phasing jobs: ~10hrs
-slurmcmd="sbatch --account=p33 --time=4-00:00:00 --mem-per-cpu=2048M --cpus-per-task=4" #1536
+slurmcmd="sbatch --account=p33"
 
 genmap=${BASEDIR}/lib/data/genetic_map_hg19_withX.txt.gz
 
 inprefix=/cluster/projects/p33/data/genetics/rawdata/genotypes/postQC/NORMENT/norment_batch3_Jan15_qc
-outprefix=/cluster/projects/p33/nobackup/tmp/test_batch3
+outprefix=/cluster/projects/p33/nobackup/tmp/fk/batch3/batch3
 tmpprefix=${outprefix}_tmp
 phaserefprefix=/cluster/projects/p33/users/franbe/norment_2018/ega.grch37.chr
 imputerefprefix=/cluster/projects/p33/data/genetics/external/HRC/decrypted/ega.grch37.chr
+refprefix=/cluster/projects/p33/nobackup/tmp/ref/hrc_refhaps
 scriptprefix=${tmpprefix}_script
 
-chromosomes=$( seq 13 19 )
-igroupsize=650 # num_samples*22 / (400[=max_jobs] - 3[=num_of_chr-wise-jobs]*22[=num_of_chr])
+chromosomes=$( seq 1 22 )
+igroupsize=650 # TODO: auto-compute - num_samples*22 / (400[=max_jobs] - 3[=num_of_chr-wise-jobs]*22[=num_of_chr])
 eaglexec=${BASEDIR}/lib/3rd/eagle
-minimacexec=${BASEDIR}/lib/3rd/minimac3
+minimacexec=${BASEDIR}/lib/3rd/Minimac3_march-sb_omp
+bcftoolsexec=${BASEDIR}/lib/3rd/bcftools
+plinkexec=${BASEDIR}/lib/3rd/plink
+timexec='/usr/bin/time -o /dev/stdout -f "ResStats\tRealSec:\t%e\tMaxMemKB:\t%M\tUserSec:\t%U\tSysSec:\t%S"'
 
-getbpstart() {
-  local -r chr=$1
-  local -r bim=$2
-  sort -t $'\t' -k 1,1 ${bim} \
-    | join -t $'\t' <( echo $chr ) - \
-    | cut -f 4 \
-    | sort -n \
-    | head -n 1
-}
+#-------------------------------------------------------------------------------
 
-getbpend() {
-  local -r chr=$1
-  local -r ifn=$2
-  sort -t $'\t' -k 1,1 ${bim} \
-    | join -t $'\t' <( echo $chr ) - \
-    | cut -f 4 \
-    | sort -nr \
-    | head -n 1
-}
+# convert
 
-if [ ! -s ${tmpprefix}.bcf -o ! -s ${tmpprefix}.bcf.csi ] ; then
-  module load plink
-  plink --bfile ${inprefix} --recode vcf bgz --out ${tmpprefix}
-  bcftools convert -Ob ${tmpprefix}.vcf.gz > ${tmpprefix}_out.bcf
-  bcftools index ${tmpprefix}_out.bcf
-  mv ${tmpprefix}_out.bcf ${tmpprefix}.bcf
-  mv ${tmpprefix}_out.bcf.csi ${tmpprefix}.bcf.csi
+cat > ${scriptprefix}0_conv.sh << EOI
+#!/usr/bin/env bash
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=12:00:00
+
+set -Eeou pipefail
+source /cluster/bin/jobsetup
+if [ -e ${tmpprefix}.bcf.csi ] ; then
+  printf "input files ready for phasing - skipping...\\n"
+  exit 0
 fi
+num_cpus_detected=\$(cat /proc/cpuinfo | grep "model name" | wc -l)
+num_cpus=\${OMP_NUM_THREADS:-\${num_cpus_detected}}
+${timexec} ${plinkexec} \\
+  --threads \${num_cpus} \\
+  --memory 30000 \\
+  --bfile ${inprefix} \\
+  --recode vcf bgz \\
+  --out ${tmpprefix}
+${bcftoolsexec} convert -O b ${tmpprefix}.vcf.gz > ${tmpprefix}_out.bcf
+${bcftoolsexec} index ${tmpprefix}_out.bcf
+mv ${tmpprefix}_out.bcf     ${tmpprefix}.bcf
+mv ${tmpprefix}_out.bcf.csi ${tmpprefix}.bcf.csi
+EOI
+
+#-------------------------------------------------------------------------------
+
+# phase
 
 #TODO: remove QC'd away samples from bio before splitting
 cut -f 1 ${inprefix}.bio | tail -n +2 | split -d -l ${igroupsize} /dev/stdin ${tmpprefix}_sample
@@ -60,6 +68,15 @@ tmpsamplelist=$( ls "${tmpprefix}_sample"* | grep 'sample[0-9]\+$' )
 for chr in ${chromosomes} ; do
 cat > ${scriptprefix}1_phase_chr${chr}.sh << EOI
 #!/usr/bin/env bash
+#SBATCH --cpus-per-task=10
+#SBATCH --mem-per-cpu=2G
+#SBATCH --time=06:00:00
+
+# Eagle's parallelization is very efficient. colossus machines seem to 
+# have 20 cpus but 40 hyperthreading cores. Therefore we use 
+# num_cpus*2 threads.
+# Running time: From 18min to 3h for batch3 (9.9k samples)
+
 set -Eeou pipefail
 source /cluster/bin/jobsetup
 if [ -e "${tmpprefix}_chr${chr}_phased.vcf.gz" ]; then
@@ -68,45 +85,37 @@ if [ -e "${tmpprefix}_chr${chr}_phased.vcf.gz" ]; then
 fi
 num_cpus_detected=\$(cat /proc/cpuinfo | grep "model name" | wc -l)
 num_cpus=\${OMP_NUM_THREADS:-\${num_cpus_detected}}
-  time ${eaglexec} \\
-    --chrom ${chr} \\
-    --geneticMapFile ${genmap} \\
-    --vcfRef ${phaserefprefix}${chr}.haplotypes.bcf \\
-    --vcfTarget ${tmpprefix}.bcf \\
-    --outPrefix ${tmpprefix}_chr${chr}_phasing \\
-    --numThreads \${num_cpus}
+${timexec} ${eaglexec} \\
+  --chrom ${chr} \\
+  --geneticMapFile ${genmap} \\
+  --vcfRef ${phaserefprefix}${chr}.haplotypes.bcf \\
+  --vcfTarget ${tmpprefix}.bcf \\
+  --outPrefix ${tmpprefix}_chr${chr}_phasing \\
+  --numThreads \$(( num_cpus * 2))
 mv ${tmpprefix}_chr${chr}_phasing.vcf.gz ${tmpprefix}_chr${chr}_phased.vcf.gz
 EOI
 done
 
-#TODO? remove me
-for chr in ${chromosomes} ; do
-cat > ${scriptprefix}2_refconv_chr${chr}.sh << EOI
-#!/usr/bin/env bash
-set -Eeou pipefail
-source /cluster/bin/jobsetup
-if [ -e "${tmpprefix}_refhaps_chr${chr}.m3vcf.gz" ]; then
-  printf "m3vcf files present - skipping...\\n"
-  exit 0
-fi
-num_cpus_detected=\$(cat /proc/cpuinfo | grep "model name" | wc -l)
-num_cpus=\${OMP_NUM_THREADS:-\${num_cpus_detected}}
-    time ${minimacexec} \\ 
-      --refHaps ${imputerefprefix}${chr}.haplotypes.vcf.gz \\
-      --processReference \\
-      --prefix ${tmpprefix}_refhaps_chr${chr}_out \\
-      --cpus \${num_cpus} \\
-      --rounds 5
-    mv ${tmpprefix}_refhaps_chr${chr}_out.m3vcf.gz \\
-       ${tmpprefix}_refhaps_chr${chr}.m3vcf.gz \\
-EOI
-done
+#-------------------------------------------------------------------------------
+
+# impute
 
 for chr in ${chromosomes} ; do
   for samplefile in ${tmpsamplelist} ; do
     sample=${samplefile##*_}
 cat > ${scriptprefix}3_impute_chr${chr}_${sample}.sh << EOI
 #!/usr/bin/env bash
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=12:00:00
+
+# Minimac3-omp cpus=2, mem-per-cpu=16 + lowMem flag works fine with hrc-32k
+# (23G mem usage and 300min wall clock time for a chr2 n=650 sample) 
+# Number of samples does not seem to increase mem usage.
+# Mem usage seems to be determined by ref file.
+#
+# TODO:try cpu=4 and mem-per-cpu=8 + lowMem flag
+
 set -Eeou pipefail
 source /cluster/bin/jobsetup
 if [ -e "${tmpprefix}_chr${chr}_${sample}_imputed.dose.vcf.gz" ]; then
@@ -115,48 +124,64 @@ if [ -e "${tmpprefix}_chr${chr}_${sample}_imputed.dose.vcf.gz" ]; then
 fi
 num_cpus_detected=\$(cat /proc/cpuinfo | grep "model name" | wc -l)
 num_cpus=\${OMP_NUM_THREADS:-\${num_cpus_detected}}
-    bcftools view -S ${samplefile} -Oz \\
-      --force-samples ${tmpprefix}_chr${chr}_phased.vcf.gz \\
-      > ${tmpprefix}_chr${chr}_${sample}_phased.vcf.gz
-    time ${minimacexec} \\
-      --cpus \${num_cpus} \\
-      --haps ${tmpprefix}_chr${chr}_${sample}_phased.vcf.gz \\
-      --refHaps ${tmpprefix}_refhaps_chr${chr}.m3vcf.gz \\
-      --rounds 5 --states 200 \\
-      --noPhoneHome \\
-      --prefix ${tmpprefix}_chr${chr}_${sample}_imputing \\
-      > ${tmpprefix}_chr${chr}_${sample}_imputing.log 2>&1
-    bcftools index ${tmpprefix}_chr${chr}_${sample}_imputing.dose.vcf.gz
-    # fix minimac3 output
-    bcftools view -h ${tmpprefix}_chr${chr}_${sample}_imputing.dose.vcf.gz \\
-      | awk -v genofilter='##FILTER=<ID=GENOTYPED,Description="Site was genotyped">' \\
-        'BEGIN{ flag = 0 } {
-          if ( \$0 ~ "^##FILTER" ) {
-            if ( flag==0 ) print( genofilter )
-            flag = 1
-          }
-          print
-        }' \\
-      > ${tmpprefix}_chr${chr}_${sample}_imputing_hdrpatch
-    bcftools reheader -h ${tmpprefix}_chr${chr}_${sample}_imputing_hdrpatch \\
-      ${tmpprefix}_chr${chr}_${sample}_imputing.dose.vcf.gz \\
-      > ${tmpprefix}_chr${chr}_${sample}_imputing_hdrpatch.dose.vcf.gz
-    mv \\
-      ${tmpprefix}_chr${chr}_${sample}_imputing_hdrpatch.dose.vcf.gz \\
-      ${tmpprefix}_chr${chr}_${sample}_imputed.dose.vcf.gz
-    bcftools index -f ${tmpprefix}_chr${chr}_${sample}_imputed.dose.vcf.gz
+${bcftoolsexec} view -S ${samplefile} -Oz \\
+  --force-samples ${tmpprefix}_chr${chr}_phased.vcf.gz \\
+  > ${tmpprefix}_chr${chr}_${sample}_phased.vcf.gz
+${timexec} ${minimacexec} \\
+  --cpus \${num_cpus} \\
+  --lowMemory \\
+  --haps ${tmpprefix}_chr${chr}_${sample}_phased.vcf.gz \\
+  --refHaps ${refprefix}_chr${chr}.m3vcf.gz \\
+  --rounds 5 --states 200 \\
+  --noPhoneHome \\
+  --prefix ${tmpprefix}_chr${chr}_${sample}_imputing \\
+  > ${tmpprefix}_chr${chr}_${sample}_imputing.log 2>&1
+${bcftoolsexec} index ${tmpprefix}_chr${chr}_${sample}_imputing.dose.vcf.gz
+# fix minimac3 output
+${bcftoolsexec} view -h ${tmpprefix}_chr${chr}_${sample}_imputing.dose.vcf.gz \\
+  | awk -v genofilter='##FILTER=<ID=GENOTYPED,Description="Site was genotyped">' \\
+    'BEGIN{ flag = 0 } {
+      if ( \$0 ~ "^##FILTER" ) {
+        if ( flag==0 ) print( genofilter )
+        flag = 1
+      }
+      print
+    }' \\
+  > ${tmpprefix}_chr${chr}_${sample}_imputing_hdrpatch
+${bcftoolsexec} reheader -h ${tmpprefix}_chr${chr}_${sample}_imputing_hdrpatch \\
+  ${tmpprefix}_chr${chr}_${sample}_imputing.dose.vcf.gz \\
+  > ${tmpprefix}_chr${chr}_${sample}_imputing_hdrpatch.dose.vcf.gz
+mv \\
+  ${tmpprefix}_chr${chr}_${sample}_imputing_hdrpatch.dose.vcf.gz \\
+  ${tmpprefix}_chr${chr}_${sample}_imputed.dose.vcf.gz
+${bcftoolsexec} index -f ${tmpprefix}_chr${chr}_${sample}_imputed.dose.vcf.gz
 EOI
   done
-cat > ${scriptprefix}4_merge_chr${chr}.sh << EOI
+done
+
+#-------------------------------------------------------------------------------
+
+# merge
+
+for chr in ${chromosomes} ; do
+  cat > ${scriptprefix}4_merge_chr${chr}.sh << EOI
 #!/usr/bin/env bash
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=4G
+#SBATCH --time=06:00:00
+
 set -Eeou pipefail
 source /cluster/bin/jobsetup
-  bcftools merge ${tmpprefix}_chr${chr}_*_imputed.dose.vcf.gz -Oz \\
-    > ${tmpprefix}_chr${chr}_imputed.dose.vcf.gz
-  mv ${tmpprefix}_chr${chr}_imputed.dose.vcf.gz \\
-    ${outprefix}_chr${chr}_imputed.dose.vcf.gz
+${bcftoolsexec} merge ${tmpprefix}_chr${chr}_*_imputed.dose.vcf.gz -Oz \\
+  > ${tmpprefix}_chr${chr}_imputed.dose.vcf.gz
+mv ${tmpprefix}_chr${chr}_imputed.dose.vcf.gz \\
+   ${outprefix}_chr${chr}_imputed.dose.vcf.gz
 EOI
 done
+
+#-------------------------------------------------------------------------------
+
+# exec
 
 echo 'submitting scripts..'
 
