7a1c5ee2a3606fa55811c20abd4c6dcb7dd384e7
Francesco Bettella <checco74@gmail.com>
2019-05-10 18:06:04 +0200

    prepare to publish

--- gitsync/pushgithub.sh
+++ gitsync/pushgithub.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-git remote add origin_gh_norm git@github.com:precimed/genimpute.git
+git remote add origin_gh_norm git@github.com:scimerc/genimpute.git
 git push origin_gh_norm
+
