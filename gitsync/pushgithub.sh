2a1e1bc2248ca9a3f7b17753fa2f79fc44b26294
Francesco Bettella <checco74@gmail.com>
2019-09-09 18:05:17 +0200

    squashed bug

--- gitsync/pushgithub.sh
+++ gitsync/pushgithub.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
-if [ "$1" != "" ] ; then
+if [ "$1" == "" ] ; then
   echo "no public repository specified."
   echo "usage: $( basename $0 ) <path_to_repository>"
   echo " e.g.: $( basename $0 ) https://github.com/scimerc/genimpute.git"
+  exit 0
 fi
 
 PUBREP="$1"
 PRJDIR="$( cd $( dirname $( readlink -f "$0" ) )/../ ; pwd )"
 
-git lfs track ${PRJDIR}/lib/data/genetic_map_b37_withX.txt.gz
-git add ${PRJDIR}/lib/data/genetic_map_b37_withX.txt.gz
-git add ${PRJDIR}/.gitattributes
+cd ${PRJDIR}
+
+git lfs track lib/data/genetic_map_b37_withX.txt.gz
+git add lib/data/genetic_map_b37_withX.txt.gz
+git add .gitattributes
 git commit -m 'lfs'
 
 git remote add origin_gh_norm ${PUBREP}
