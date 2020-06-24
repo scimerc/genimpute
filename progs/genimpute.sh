f8ec7916af716aaee4d4c89f6ebe09212f4bd8a2
Florian Krull <flo.krull@gmail.com>
2020-06-24 16:40:22 +0200

    Revert "genimpute.sh: use busybox time"
    This reverts commit f96d7e8f58063096ea2b9f6a64ad9439df92ea23.

--- progs/genimpute.sh
+++ progs/genimpute.sh
@@ -162,8 +162,10 @@ export -f printlog
 declare jobdepflag=""
 declare joblist=""
 
-declare -r timeformat="ResStats_MaxMemKB=%M_RealTimeSec=%e_UserTimeSec=%U_SysTimeSec=%S"
-declare -r timexec="${BASEDIR}/lib/3rd/busybox/usr/bin/time -o /dev/stdout -f ${timeformat}"
+#TODO: find the time executable in the cluster and restore the original time commands
+declare -r timeformat="ResStats\nMaxMem_KB\t%M\nRealTime_s\t%e\nUserTime_s\t%U\nSysTime_s\t%S"
+declare -r timexec="${BASEDIR}/lib/3rd/time -o /dev/stdout -f ${timeformat}"
+# declare -r timexec="/usr/bin/env time -o /dev/stdout -f ${timeformat}"
 export timexec
 
 declare -r cfg_plinkctime=$( cfgvar_get plinkctime )
