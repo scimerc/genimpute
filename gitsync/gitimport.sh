bd59c1339386f9cdc7fa3df28b8a1f1349059a8d
Francesco Bettella <checco74@gmail.com>
2020-07-16 15:07:58 +0200

    gitsync/gitimport.sh: avoid syncing lfs files

--- gitsync/gitimport.sh
+++ gitsync/gitimport.sh
@@ -4,9 +4,9 @@
 # exit on error
 set -ETeuo pipefail
 
-[ -d ".git" ] || { printf "error at %d.\n" $LINENO >&2; exit 1; }
+[ -d ".git" ] || { printf "Error at %d.\n" $LINENO >&2; exit 1; }
 
-[ "$*" != "" ] || { printf "usage: %s <tarfile>\n" ${BASH_SOURCE[0]}; exit 0; }
+[ "$*" != "" ] || { printf "Usage: %s <tarfile>\n" ${BASH_SOURCE[0]}; exit 0; }
 
 TARFILE="$1"
 
@@ -19,14 +19,17 @@ PRJDIR=$( readlink -e $PRJDIR )
 SELFABS=$( readlink -e "${BASH_SOURCE[0]}" )
 SELFREL="${SELFABS#${PRJDIR}/}"
 
+# get list of large files (not to be synced)
+LFSLIST=( $( git lfs ls-files | cut -d ' ' -f 3 ) )
+
 echo "Extracting source archive '${TARFILE}'..."
 cd ${LOCTMPDIR} && rm -rf ${PRJNAME}
 tar xvf "${TARFILE}"
 
-[ -d ${PRJNAME} ] || { printf "project '%s' not found.\n" ${PRJNAME}; exit 1; }
+[ -d ${PRJNAME} ] || { printf "Project '%s' not found.\n" ${PRJNAME}; exit 1; }
 
 echo "Syncing into '${PRJDIR}'..."
-rsync -av --delete --exclude={".git","${SELFREL}"} ${PRJNAME}/ ${PRJDIR}/
+rsync -av --delete --exclude={".git","${LFSLIST[@]}","${SELFREL}"} ${PRJNAME}/ ${PRJDIR}/
 
 [ -f ${PRJNAME}/${SELFREL} ] || { echo "${SELFREL}: Not daring to self-destroy."; exit 0; }
 cmp $SELFABS ${PRJNAME}/${SELFREL} 2>/dev/null || { echo "${SELFREL}: Not daring to self-sync."; }
